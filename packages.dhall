b45837eaf570614006d959235a39713d9ee6c196
AlbertoPerezDepop <albertoperez1994@gmail.com>
2023-03-29 17:22:50 +0200

    Initial commit

---
+++ packages.dhall
@@ -0,0 +1,4 @@
+let upstream =
+      https://github.com/purescript/package-sets/releases/download/psc-0.15.4-20220816/packages.dhall sha256:8b4467b4b5041914f9b765779c8936d6d4c230b1f60eb64f6269c71812fd7e98
+
+in  upstream
---
+++ src/Main.purs
@@ -0,0 +1,13 @@
+module Main where
+
+import Prelude
+
+import App.Button as Button
+import Effect (Effect)
+import Halogen.Aff as HA
+import Halogen.VDom.Driver (runUI)
+
+main :: Effect Unit
+main = HA.runHalogenAff do
+  body <- HA.awaitBody
+  runUI Button.component unit body
