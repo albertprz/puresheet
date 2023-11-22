4a26edc1f7507c60e5dd4daf43ba31ae92d9dcd9
Alberto Perez <albertoperez1994@gmail.com>
2023-11-22 10:14:03 +0100

    Included MinLenVect

---
+++ src/Utils/MinLenVect.js
@@ -0,0 +1,9 @@
+use "strict"
+
+export const indexImpl = (i) => (v) => v[i];
+export const modifyImpl = (i) => (f) => (v) => {
+  const r = f(v[i]);
+  const a = v.slice();
+  a[i] = r;
+  return a;
+}
